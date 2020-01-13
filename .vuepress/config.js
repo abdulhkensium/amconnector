40f85e1a6d7eb919fc62971cbd90660d15c80157
Abdul Hafeez <hafeez3000@gmail.com>
2020-01-13 14:32:17 +0530

    Added Menu

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,24 @@
 module.exports = {
+
+    title: 'Connector Documentation',
+    description: 'Acumatica Magento Connector Documentation',
+    head: [
+        ['link', { rel: 'icon', href: '/favicon.png' }]
+      ],
+    
+      themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'About', link: '/about/' }, 
+          { text: 'Setup', link: '/setup/' },
+          { text: 'Configuration', link: '/configuration/' },
+          { text: 'Mapping', link: '/mapping/' },
+          { text: 'Queue', link: '/queue/' },
+          { text: 'Logs', link: '/logs/' },
+          { text: 'Report', link: '/report/' }
+           
+          ]
+      },
    
     plugins: ['@vuepress/pwa', {
         serviceWorker: true,
